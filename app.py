0093b1f03a396c73bba8e0945c3afada483b9aa3
arthur.luiz <arthur.luiz@dpsp.com.br>
2022-01-13 22:50:40 -0300

    app

---
+++ app.py
@@ -0,0 +1,24 @@
+import requests
+import json
+
+def buscar_dados():
+    request = requests.get("https://api.pancakeswap.info/api/v2/tokens/0x50332bdca94673F33401776365b66CC4e81aC81d")
+    # print(request.content)
+    jsonFormatado = json.loads(request.content)
+    valorCCAR = (jsonFormatado.get("data").get("price"))
+
+    priceReal = requests.get("https://economia.awesomeapi.com.br/json/last/USD")
+    jsonReal = json.loads(priceReal.content)
+    valorReal = (jsonReal.get("USDBRL").get("high"))
+
+    valorCCARfloat = float(valorCCAR) 
+
+    valorRealfloat = float(valorReal)
+
+    conversao = valorCCARfloat * valorRealfloat
+
+    print(f'A cotação de um CCAR para o valor do REAL atualmente é : R$%.2f' %conversao )
+
+
+if __name__ == '__main__':
+    buscar_dados()
